9c32fc0848790e9145c4b27f4f0eb1810b971e19
Keyzou <keyzou95@gmail.com>
2019-01-25 22:28:20 +0100

    Updates

--- precache-manifest.1656c66b789495734d2aeea3056e43a8.js
+++ precache-manifest.1656c66b789495734d2aeea3056e43a8.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/countries/static/js/runtime~main.a8eff2cf.js"
   },
   {
-    "revision": "ea91fbe9f801b737c1ca",
-    "url": "/countries/static/js/main.ea91fbe9.chunk.js"
+    "revision": "71723ec92279e483ee0b",
+    "url": "/countries/static/js/main.71723ec9.chunk.js"
   },
   {
     "revision": "d8b615692d7ac05056a1",
     "url": "/countries/static/js/1.d8b61569.chunk.js"
   },
   {
-    "revision": "ea91fbe9f801b737c1ca",
+    "revision": "71723ec92279e483ee0b",
     "url": "/countries/static/css/main.e997a617.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/countries/static/css/1.7f152d54.chunk.css"
   },
   {
-    "revision": "de7bf53099c060e81603c53d92c25c0f",
+    "revision": "de0bc3881af15994c1a920c74458473c",
     "url": "/countries/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/countries/precache-manifest.d9d3396fcccc9d5316124784353744ee.js"
+  "/countries/precache-manifest.1656c66b789495734d2aeea3056e43a8.js"
 );
 
 workbox.clientsClaim();
