5cd5b38e11573aa985766c4072aeefa8be47456b
Keyzou <keyzou95@gmail.com>
2019-01-25 21:33:27 +0100

    Updates

---
+++ precache-manifest.d9d3396fcccc9d5316124784353744ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8eff2cff25b2e7b790a",
+    "url": "/countries/static/js/runtime~main.a8eff2cf.js"
+  },
+  {
+    "revision": "ea91fbe9f801b737c1ca",
+    "url": "/countries/static/js/main.ea91fbe9.chunk.js"
+  },
+  {
+    "revision": "d8b615692d7ac05056a1",
+    "url": "/countries/static/js/1.d8b61569.chunk.js"
+  },
+  {
+    "revision": "ea91fbe9f801b737c1ca",
+    "url": "/countries/static/css/main.e997a617.chunk.css"
+  },
+  {
+    "revision": "d8b615692d7ac05056a1",
+    "url": "/countries/static/css/1.7f152d54.chunk.css"
+  },
+  {
+    "revision": "de7bf53099c060e81603c53d92c25c0f",
+    "url": "/countries/index.html"
+  }
+];
